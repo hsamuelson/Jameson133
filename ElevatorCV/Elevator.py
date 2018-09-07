90ef9d9eea9861cf1756bb97e2fc78613eeb6f5c
Samik Shrotriya <ss2659@cornell.edu>
2018-09-06 22:43:17 -0400

    initial commit, basic framework
    Elevator.py wrong

---
+++ ElevatorCV/Elevator.py
@@ -0,0 +1,13 @@
+
+class Elevator():
+    floors = 0
+    waiting_time = 0
+    total_waiting_time = 0
+
+
+
+    def __init__(self):
+        self.hi = 0
+
+
+
---
+++ ElevatorCV/Person.py
@@ -0,0 +1,28 @@
+class Person:
+
+    def __init__(self, arrival_floor, destination_floor):
+        self._start = arrival_floor
+        self._end = destination_floor
+        self._timer = 0
+
+    def move_one_floor(self):
+        self._timer = self._timer + 5
+
+    def stop_at_floor(self):
+        self._timer = self._timer + 10
+
+    def set_start(self, floor):
+        self._start = floor
+
+    def set_end(self, floor):
+        self._end = floor
+
+    def get_start(self):
+        return self._start
+
+    def get_end(self):
+        return self._end
+
+    def get_timer(self):
+        return self._timer
+
